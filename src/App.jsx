704d2e785d1713fbe9d2301ff58bb54a2e4f4a05
JaRoD41 <laurent.trinco@gmail.com>
2025-10-08 13:02:21 +0200

    mise en place de la page principale avec les onglets

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
 import { useState } from 'react'
-
-import Header from './components/Home/Header/Header'
-import WelcomeCard from './components/Home/WelcomeCard/WelcomeCard'
-import Services from './components/Home/Services/Services'
 import NavBar from './components/Shared/NavBar/NavBar'
-
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Work from './components/Work/Work'
+import Contact from './components/Contact/Contact'
 import './App.css'
 
 const App = () => {
 	const [activeTab, setActiveTab] = useState('home')
 
-	const services = [
-		{
-			title: 'Web Development',
-			description: 'I have experience with React, Next.js, and Gatsby.',
-		},
-		{
-			title: 'Data Science',
-			description: 'I have experience with Python, SQL, and MongoDB.',
-		},
-		{
-			title: 'UI/UX Design',
-			description: 'I have experience with Figma, Sketch, and Adobe XD.',
-		},
-	]
 	return (
-		<>
-			<div className='app'>
-				<Header name='Jarod' title='Software Developer' imgUrl='https://avatars.githubusercontent.com/u/10406239?v=4' />
-				<main className='main-content'>
-					<WelcomeCard message='Welcome to my portfolio!' />
-					<Services services={services} />
-				</main>
-
-				<NavBar activeTab={activeTab} onTabChange={setActiveTab} />
-			</div>
-		</>
+		<div className='app'>
+			{activeTab === 'home' && <Home />}
+			{activeTab === 'about' && <About />}
+			{activeTab === 'work' && <Work />}
+			{activeTab === 'contact' && <Contact />}
+			<NavBar activeTab={activeTab} onTabChange={setActiveTab} />
+		</div>
 	)
 }
 
