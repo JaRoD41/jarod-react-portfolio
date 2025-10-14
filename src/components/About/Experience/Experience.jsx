b476956f7ac8e5a87f925701e035258730d9aef0
JaRoD41 <laurent.trinco@gmail.com>
2025-10-14 11:45:21 +0200

    fin premier jet About

--- src/components/About/Experience/Experience.jsx
+++ src/components/About/Experience/Experience.jsx
@@ -1,6 +1,25 @@
 import './experience.css'
+import experiences from '../../../data/experiences.json'
 
 const Experience = () => {
-	return <div>Experience</div>
+	return (
+		<section className='experience-section'>
+			<h2 className='section-title'>Experience</h2>
+			{experiences.map((exp) => (
+				<div key={exp.title} className='experience-card'>
+					<div className='experience-header'>
+						<i className='fas fa-briefcase'></i>
+						<div>
+							<h3>{exp.title}</h3>
+							<p className='company'>
+								{exp.company} • {exp.period}
+							</p>
+						</div>
+					</div>
+					<p className='experience-description'>{exp.description}</p>
+				</div>
+			))}
+		</section>
+	)
 }
 export default Experience
